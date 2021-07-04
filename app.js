f8ba7f76d6bbe6891c827fa8b6249b368d7fb28d
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-07-04 16:01:56 +0300

    new

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ console.log(age)
 alert("And you are " + age + " years old")
 
 let color = prompt("Favorite color ?")
+console.log(color)
 alert("your favorite color is " + color)
 
 let food = prompt("Favorite food ?")
+console.log(food)
 alert("your favorite food is " + food)
 
-alert("You are " + userName + "You are " + age +" years old" + " your favorite color is " + color + " and your favorite food is " + food)
+alert("You are " + userName + " You are " + age +" years old" + " your favorite color is " + color + " and your favorite food is " + food)
