930a9dd4297b6df33c3f9bdee760cf62ec399fea
Nedal1994 <85867422+Nedal1994@users.noreply.github.com>
2021-07-04 15:13:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let userName=prompt("What's your name ?")
 console.log(userName)
-alert("Welcome "+userName)
+alert("Welcome "+ userName)
 
 let age = prompt("How old are you ?")
 console.log(age)
-alert("And you are " +age+ " years old")
+alert("And you are " + age + " years old")
 
 let color = prompt("Favorite color ?")
 console.log(color)
-alert("your favorite color is " +color)
+alert("your favorite color is " + color)
